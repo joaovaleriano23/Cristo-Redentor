1e96da4727196d4f6e38a31b9238e156fcedcdad
joaovaleriano23 <joao.valeriano@gmail.com>
2025-07-13 00:11:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const abriMenu = document.querySelector('nav .nav-list')
+const clouse = document.querySelector('nav .nav-list')
+
+function openMenu(){
+    abriMenu.classList.add('open')
+}
+
+console.log (abriMenu)
+
+function closeMenu(){
+    clouse.classList.remove('open')
+}
+
+console.log (clouse)
